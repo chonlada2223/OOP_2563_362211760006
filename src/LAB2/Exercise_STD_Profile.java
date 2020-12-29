e22994ef42bb89ced6d578fcbf505aae01fa0b97
chonlada <chonlykupp@rmutsvmail.com>
2020-12-29 11:37:31 +0700

    Leb 2: type and import data

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,39 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter message: ");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Enter an integer: ");
+        String std_id = scanner.next();
+
+
+        System.out.print("Enter an integer: ");
+        int Age = scanner.nextInt();
+
+
+        System.out.println("Enter an integer: ");
+        String tel = scanner.next();
+
+
+        System.out.println("Enter an message: ");
+        String email = scanner.next();
+
+
+
+
+    }
+
+
+
+
+
+}
