88db17cebe76276af5726fdbf6b218d0d35dc576
assignment <chonlykupp@rmutsvmail.com>
2021-01-06 19:55:41 +0700

    UpdateData file

---
+++ src/LAB3/ex_for.java
@@ -0,0 +1,15 @@
+package LAB3;
+
+public class ex_for {
+    public static void main(String[] args) {
+        //พิมพ์ข้อความ Hello 10ครั้ง
+        for (int i = 1; i < 10 ; i++)
+            System.out.println("Hello" + i);
+            System.out.println("MIT");
+
+        System.out.println("Good Bye");
+
+
+
+    }//main
+}//class
